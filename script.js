9fd620bb3fbb784ab708971383ef1a7e41d1e598
Suzui235 <86270957+Suzui235@users.noreply.github.com>
2026-01-15 21:29:19 -0700

    Primera versión del portafolio

---
+++ script.js
@@ -0,0 +1,11 @@
+function mostrarContacto() {
+    const contacto = document.getElementById("dato-contacto");
+    contacto.style.display = "block";
+}
+
+function scrollHabilidades() {
+    document.getElementById("habilidades").scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
